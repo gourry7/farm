76ed1807aa00fff9f0bdb1848524501eac5c0a14
gourry7 <55119142+gourry7@users.noreply.github.com>
2025-05-12 10:11:34 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // Firebase 설정
 export const firebaseConfig = {
-    apiKey: "AIzaSyDFyoSOXpF32S9HHENuJ_Ogr7bW50LG2wE",
-    authDomain: "farm-server-f0f8c.firebaseapp.com",
-    databaseURL: "https://farm-server-f0f8c-default-rtdb.firebaseio.com",
-    projectId: "farm-server-f0f8c",
-    storageBucket: "farm-server-f0f8c.appspot.com",
-    appId: "1:872192710214:web:6587f7631ba7cdde36dc47"
+    apiKey: process.env.FIREBASE_API_KEY || "YOUR_API_KEY",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
+    databaseURL: process.env.FIREBASE_DATABASE_URL || "YOUR_DATABASE_URL",
+    projectId: process.env.FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
+    appId: process.env.FIREBASE_APP_ID || "YOUR_APP_ID"
 }; 
