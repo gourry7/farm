94877b394a664519cef5ee5afce4ca78b1663874
gourry7 <55119142+gourry7@users.noreply.github.com>
2025-05-12 09:57:19 +0900

    Delete web directory

--- web/config.js
+++
@@ -1,9 +0,0 @@
-// Firebase 설정
-export const firebaseConfig = {
-    apiKey: "AIzaSyDFyoSOXpF32S9HHENuJ_Ogr7bW50LG2wE",
-    authDomain: "farm-server-f0f8c.firebaseapp.com",
-    databaseURL: "https://farm-server-f0f8c-default-rtdb.firebaseio.com",
-    projectId: "farm-server-f0f8c",
-    storageBucket: "farm-server-f0f8c.appspot.com",
-    appId: "1:872192710214:web:6587f7631ba7cdde36dc47"
-}; 
